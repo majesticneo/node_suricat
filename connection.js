9442429698ebeae6a68141cea143380116da17fe
majesticneo <majesticneo@hotmail.com>
2016-12-18 13:57:09 +0100

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,31 @@
+/**
+ * module to connect to mysql database
+ * database is store on Lionel desktop
+ */
+var mysql = require('mysql');
+ 
+function Connection() 
+{
+  this.pool = null;
+ 
+  this.init = function() 
+  {
+    this.pool = mysql.createPool({
+      connectionLimit: 10,
+      host: 'localhost',
+      user: 'cdi',
+      password: 'cdi',
+      database: 'suricat'
+    });
+  };
+ 
+  this.acquire = function(callback) 
+  {
+    this.pool.getConnection(function(err, connection) 
+    {
+      callback(err, connection);
+    });
+  };
+}
+ 
+module.exports = new Connection();
