6417515514ebc1f74c009f07566edc5e6d581598
Lionel <majesticneo@hotmail.com>
2016-12-29 16:20:44 +0100

    29/12 Lionel / features :
    implement delete belongto to remove user from team

--- app.js
+++ app.js
@@ -20,11 +20,8 @@ app.options('*', cors());
 
 /*--Parser --*/
 app.use(cookieParser());
-app.use(bodyParser.json({limit: '5mb'}));
-app.use(bodyParser.urlencoded({
- limit: '150mb',
- extended: true 
-}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 /*-- Allow CORS for cross origin--*/
